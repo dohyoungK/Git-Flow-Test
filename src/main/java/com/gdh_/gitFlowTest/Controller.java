e39d14246b57161f6b906c2140ee713f71edecc0
dohyoungK <gdh012@pusan.ac.kr>
2023-08-07 15:43:45 +0900

    initial

---
+++ src/main/java/com/gdh_/gitFlowTest/Controller.java
@@ -0,0 +1,13 @@
+package com.gdh_.gitFlowTest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "To-do Application";
+    }
+}
---
+++ src/main/java/com/gdh_/gitFlowTest/GitFlowTestApplication.java
@@ -0,0 +1,13 @@
+package com.gdh_.gitFlowTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitFlowTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitFlowTestApplication.class, args);
+	}
+
+}

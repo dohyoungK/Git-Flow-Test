ecb486bcc9e747a8e41618bcc44362f1ffb39c47
dohyoungK <gdh012@pusan.ac.kr>
2023-08-07 16:46:54 +0900

    controller 구현

--- src/main/java/com/gdh_/gitFlowTest/RecommendationController.java
+++ src/main/java/com/gdh_/gitFlowTest/RecommendationController.java
@@ -4,8 +4,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Controller {
-
+public class RecommendationController {
     @GetMapping("/")
     public String helloWorld() {
         return "To-do Application";
